12590f10eea8918881455d4a0935addb6a75f224
Christopher Dirksen <chrisdirksen024@outlook.com>
2022-09-27 18:23:40 -0400

    commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var button = document.getElementsByTagName('button')
+var favorites = 
+function saveFav(){
+   var list = document.createElement('li');
+   var fav = document.createTextNode('object source')
+   list.appendChild(fav)
+   document.getEelementById('drinkFav').appendChild('list')
+}
+document.getElementById(favBar).appendChild
