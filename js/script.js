34b77c288904fc44290b840f5943fe88ae97c19c
exonerated <jordansmithofficially@gmail.com>
2022-10-01 20:15:47 -0400

    added extra html and javascript tweaks and some css

--- js/script.js
+++ js/script.js
@@ -1,12 +1,57 @@
-var spoonacularAPIKey = "af28c41e911742cab678a8ddcd93acaa";
+// API KEY : eeb9c6fafcaf4013839d8c8e6162013a
+var spoonacularAPIKey = "eeb9c6fafcaf4013839d8c8e6162013a";
 // var cocktailAPIKey = "KxE5FMqtJE+cddRT34B6Vw==Qj3At9bRl1rlRw1Z";
 var cocktailAPIKey = "d9d1de419cmsh3efe3d08141101ap1d1cdejsnd21e801ce08b"
-var dinnerChoice = document.getElementById("dinnerInput").value;
+// var dinnerChoice = document.getElementById("dinnerInput").value;
 var dinnerBtn = document.getElementById("dinnerBtn");
-var cocktailChoice = document.getElementById("cocktailInput").value;
+// var cocktailChoice = document.getElementById("cocktailInput").value;
 var cocktailBtn = document.getElementById("cocktailBtn");
 var rightCard = document.getElementById("right");
 var leftCard = document.getElementById("left");
+
+var dishForm = document.querySelector(".dish-form");
+var dishInput = document.querySelector(".dish-input");
+var dishesResult = document.querySelector(".dishes-results");
+dishForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let dishValue = dishInput.value;
+    dishValue = dishValue.split(" ").join(",+");
+
+    const options = {method: 'GET'};
+    // ​https://api.spoonacular.com/recipes/complexSearch?apiKey=eeb9c6fafcaf4013839d8c8e6162013a&query=gluten-free
+    // https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2
+    let fetchDish = fetch('https://api.spoonacular.com/recipes/findByIngredients?apiKey='+spoonacularAPIKey+'&ingredients='+dishValue+'&number=5', options)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        return response.forEach((res) => {
+            dishesResult.innerHTML += `
+            <div class="card-info">
+                <h3>${res?.title}</h3>
+                <div class="card-body">
+                    <div class="card-img-wrapper">
+                        <img src="${res?.image}" alt="${res?.title}">
+                    </div>
+                    <div class="card-ingredients">
+                        <h4>Ingredients used were:</h4>
+                        <ul>
+                            <li>Name: ${res?.usedIngredients[0]?.name ? res?.usedIngredients[0]?.name : "Ingredient name not avaialable"}</li>
+                            <li>Amount: ${res?.usedIngredients[0]?.amount ? res?.usedIngredients[0]?.amount : "Amount not available"}</li>
+                            <li>Original: ${res?.usedIngredients[0]?.original ? res?.usedIngredients[0]?.original : "Original name not available"}</li>
+                            <li>Unit: ${res?.usedIngredients[0]?.unit ? res?.usedIngredients[0]?.unit : "Unit not available"}</li>
+                        </ul>
+                    <div>
+                </div>
+            </div>
+        `;
+
+        })
+        
+    } )
+    .catch(err => console.error(err));
+   
+      
+})
 
 // dinnerBtn.addEventListener('click', dinnerChoice);
 // cocktailBtn.addEventListener('click', cocktailChoice);
